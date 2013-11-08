d341285172a90033bba5edc24931b10a674a1d21
Gijs Molenaar <gijs@pythonic.nl>
2013-11-08 16:49:25 +0100

    switch to makefile building

--- doit.sh
+++ doit.sh
@@ -1,22 +1,10 @@
+#!/bin/bash
 
-EMCC=emcc
+cp Makefile.emcc CBLAS/Makefile.in
+cp Makefile_CBLAS_src CBLAS/src/.
+cd CBLAS
+make
 
-cd CBLAS/src
 
-for i in `ls *.c`; do
-     ${EMCC} -c $i -I../include;
-done
-
-for i in `ls *.f`; do
-    gcc-4.7 -c -S \
-        -fplugin=/usr/lib/gcc/x86_64-linux-gnu/4.7/plugin/dragonegg.so  \
-        -fplugin-arg-dragonegg-emit-ir \
-        $i;
-done
-
-for i in `ls *.s`; do
-    llvm-as $i;
-done
-
-emcc *.o *.bc -O2 -o blas.js
+#emcc *.o *.bc -O2 -o blas.js
 
